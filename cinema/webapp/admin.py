6829f55625c474d000eccb0619bc009baeeef193
Farid <thesomeq@gmail.com>
2019-03-04 16:21:30 +0600

    Добавлены модели Места, Зала, Жанра (категории фильма) и Сеанса.

--- cinema/webapp/admin.py
+++ cinema/webapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from webapp.models import Movie
+from webapp.models import Movie, Category, Hall, Seat, Show
 
 
 class MovieAdmin(admin.ModelAdmin):
@@ -8,4 +8,14 @@ class MovieAdmin(admin.ModelAdmin):
     search_fields = ['name', 'id']
 
 
+def list_admin_with_pk(*fields):
+    class PkListAdmin(admin.ModelAdmin):
+        list_display = ['pk'] + list(fields)
+    return PkListAdmin
+
+
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Category, list_admin_with_pk('name'))
+admin.site.register(Hall, list_admin_with_pk('name'))
+admin.site.register(Seat, list_admin_with_pk('hall', 'row', 'seat'))
+admin.site.register(Show, list_admin_with_pk('movie', 'hall', 'starts_at', 'ends_at'))
--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -17,11 +17,56 @@ class Movie(models.Model):
     release_date = models.DateField()
     finish_date = models.DateField(null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
+    categories = models.ManyToManyField('Category', related_name='categories', blank=True)
 
     objects = SoftDeleteManager()
 
     def get_absolute_url(self):
         return reverse('api_v1:movie-detail', kwargs={'pk': self.pk})
 
+    def get_categories_display(self):
+        return self.categories.all()
+
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Hall(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Seat(models.Model):
+    hall = models.ForeignKey(Hall, related_name='seats', on_delete=models.PROTECT)
+    row = models.CharField(max_length=10)
+    seat = models.CharField(max_length=5)
+
+    def __str__(self):
+        return "Row %s Seat %s" % (self.row, self.seat)
+
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='shows')
+    hall = models.ForeignKey(Hall, on_delete=models.PROTECT, related_name='shows')
+    starts_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    ticket_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return "%s, %s (%s - %s)" % (self.movie, self.hall,
+                                     self.starts_at.strftime('%d.%m.%Y %H:%M'),
+                                     self.ends_at.strftime('%d.%m.%Y %H:%M'))
