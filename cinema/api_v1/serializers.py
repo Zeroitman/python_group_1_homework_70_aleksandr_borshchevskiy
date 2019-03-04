d1b5048f8277f9d981422f8bd3d59f6c418baa01
Farid <thesomeq@gmail.com>
2019-03-04 16:47:30 +0600

    Модели Жанр, Зал, Место и Показ (сеанс) подключены в API.

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -1,10 +1,42 @@
-from webapp.models import Movie
+from webapp.models import Movie, Category, Hall, Seat, Show
 from rest_framework import serializers
 
 
-class MovieSerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
+    class Meta:
+        model = Category
+        fields = ('url', 'id', 'name', 'description')
+
+
+class MovieSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
 
     class Meta:
         model = Movie
-        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date')
+        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'categories')
+
+
+class HallSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:hall-detail')
+
+    class Meta:
+        model = Hall
+        fields = ('url', 'id', 'name', 'description')
+
+
+class SeatSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:seat-detail')
+
+    class Meta:
+        model = Seat
+        fields = ('url', 'id', 'row', 'seat', 'hall')
+
+
+class ShowSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='api_v1:category-detail')
+
+    class Meta:
+        model = Show
+        fields = ('url', 'id', 'movie', 'hall', 'starts_at', 'ends_at')
