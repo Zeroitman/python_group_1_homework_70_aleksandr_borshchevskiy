a320d24a190c3ff4ea50c704cc99e20a9a343376
Farid <thesomeq@gmail.com>
2019-03-05 18:34:57 +0600

    Добавлены вывод вложенных категорий и ссылки на связанные поля (ForeignKey, ManyToMany). Добавлен пример вывода данных по обратной связи внешнего ключа (вывод списка мест в зале, у которых указан этот зал во внешнем ключе).

--- cinema/api_v1/serializers.py
+++ cinema/api_v1/serializers.py
@@ -10,33 +10,67 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('url', 'id', 'name', 'description')
 
 
+# Сериализатор для модели категорий, предназначенный для включения в сериализатор фильмов
+# не выводит ненужные в данном случае поля: description и url
+class InlineCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
 class MovieSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:movie-detail')
 
+    # поле - вложенный сериализатор - для вывода категорий в виде списка объектов.
+    # такие поля обычно предназначены только для чтения, если не требуется одновременное
+    # создание или обновление основного и связанного объекта (связанных объектов).
+    categories = InlineCategorySerializer(many=True, read_only=True)
+
+    # второе поле только для записи (write_only=True)
+    category_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        queryset=Category.objects.all(),
+        source='categories'
+    )
+
     class Meta:
         model = Movie
-        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'categories')
+        fields = ('url', 'id', 'name', 'description', 'poster', 'release_date', 'finish_date', 'categories', 'category_ids')
+
+
+class InlineSeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = ('id', 'row', 'seat')
 
 
 class HallSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:hall-detail')
 
+    # поле, представляющее обратную связь от зала к местам в зале.
+    # название поля должно совпадать с related_name внешнего ключа от мест к залу.
+    seats = InlineSeatSerializer(many=True, read_only=True)
+
     class Meta:
         model = Hall
-        fields = ('url', 'id', 'name', 'description')
+        fields = ('url', 'id', 'name', 'description', 'seats')
 
 
 class SeatSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:seat-detail')
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', read_only=True, source='hall')
 
     class Meta:
         model = Seat
-        fields = ('url', 'id', 'row', 'seat', 'hall')
+        fields = ('url', 'id', 'row', 'seat', 'hall', 'hall_url')
 
 
 class ShowSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='api_v1:show-detail')
+    movie_url = serializers.HyperlinkedRelatedField(view_name='api_v1:movie-detail', read_only=True, source='movie')
+    hall_url = serializers.HyperlinkedRelatedField(view_name='api_v1:hall-detail', read_only=True, source='hall')
 
     class Meta:
         model = Show
-        fields = ('url', 'id', 'movie', 'hall', 'starts_at', 'ends_at')
+        fields = ('url', 'id', 'movie', 'movie_url', 'hall', 'hall_url', 'starts_at', 'ends_at')
